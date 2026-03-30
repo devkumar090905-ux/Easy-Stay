6dbbd4caef402472ba035354e217daccfa8906e3
Dev Kumar <devkumar090905@gmail.com>
2026-03-30 15:39:45 +0530

    Configure for production hosting (database, build script)

--- roombooking/settings.py
+++ roombooking/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,6 +90,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+database_url = config("DATABASE_URL", default=None)
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
